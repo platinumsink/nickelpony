3713d674d121b88d1479dca1a1c0b96454fc7293
John Hedrick <john_hedrick@weci2i.com>
2015-10-27 19:09:36 -0400

    no message

--- lib/psuedorand/receiver.php
+++ lib/psuedorand/receiver.php
@@ -1,3 +1,16 @@
 <?php
 // Receiver
+function receiveRand($receipt)
+{
+	$receipt_parts = explode('|', $receipt);
+	$return_array = array();
+
+	foreach($receipt_parts as $part) {
+		if(strpos('~' !== false)) {
+			$sub_parts = explode('~', $part);
+
+			$return_array[$sub_parts[0]] = $sub_parts[1];
+		}
+	}
+}
 ?>
