86b1efafe9954ceaa989395bce2ce86155ab3bee
John Hedrick <jhedrick@scheffey.com>
2016-10-29 23:11:31 -0400

    no message

--- lib/psuedorand/sender.php
+++ lib/psuedorand/sender.php
@@ -5,6 +5,9 @@ function sendRand($send)
 	$return_send = urlencode($send);
 	$return_send = str_replace(":", "~", $return_send);
 
+	// Append identifier
+	$return_send = "st:".$return_send;
+
 	return $return_send;
 }
 ?>
